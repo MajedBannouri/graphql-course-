a04af0994c219b593a5f51434b4c2c5e475ddc43
MajedBannouri <majed.bannouri12@gmail.com>
2021-04-12 23:35:34 +0100

    comment challenge

--- src/index.js
+++ src/index.js
@@ -1,64 +1,68 @@
-import {GraphQLServer} from 'graphql-yoga'
+import { GraphQLServer } from "graphql-yoga";
 //  Demo users data
-const users = [{
-    id: '1',
-    name: 'Majed',
-    email: 'myemail@example.com'
-},
-{
-    id: '2',
-    name: 'John',
-    email: 'John@example.com'
-},
-{
-    id: '3',
-    name: 'Eva',
-    email: 'Eva@example.com',
-    
-},
-]
-const posts = [{
-    id: '1',
-    title: 'The great gatsby',
-    body: 'some text here',
+const users = [
+  {
+    id: "1",
+    name: "Majed",
+    email: "myemail@example.com",
+  },
+  {
+    id: "2",
+    name: "John",
+    email: "John@example.com",
+  },
+  {
+    id: "3",
+    name: "Eva",
+    email: "Eva@example.com",
+  },
+];
+const posts = [
+  {
+    id: "1",
+    title: "The great gatsby",
+    body: "some text here",
     published: true,
-    author: '1'
-},
-{
-    id: '2',
-    title: 'Elon Musk Hobbies',
-    body: 'some text here ',
+    author: "1",
+  },
+  {
+    id: "2",
+    title: "Elon Musk Hobbies",
+    body: "some text here ",
     published: true,
-    author: '1'
-},
-{
-    id: '3',
-    title: 'Jeff Bizos cutting edge visionary',
-    body: 'some text here ',
+    author: "1",
+  },
+  {
+    id: "3",
+    title: "Jeff Bizos cutting edge visionary",
+    body: "some text here ",
     published: true,
-    author: '2'
-},
-]
+    author: "2",
+  },
+];
 
 const comments = [
- {
-    id:'111',
-    text:"comment 1"
-},
-{
-    id:'123',
-    text:"comment 2"
-},
-{
-    id:'223',
-    text:"comment 3"
-},
-{
-    id:'332',
-    text:"comment 4"
-}
-]
-
+  {
+    id: "111",
+    text: "comment 1",
+    author: "2",
+  },
+  {
+    id: "123",
+    text: "comment 2",
+    author: "2",
+  },
+  {
+    id: "223",
+    text: "comment 3",
+    author: "1",
+  },
+  {
+    id: "332",
+    text: "comment 4",
+    author: "3",
+  },
+];
 
 // Type definitions (schema)
 const typeDefs = `
@@ -75,6 +79,7 @@ const typeDefs = `
         email: String!
         age: Int
         posts: [Post!]!
+        comments: [Comment!]!
 
 
     }
@@ -89,80 +94,88 @@ const typeDefs = `
     type Comment {
         id: ID!
         text: String!
+        author: User!
     }
-`
-    
-
-
+`;
 
 // Resolvers
 const resolvers = {
+  Query: {
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
+      }
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      }
+      return posts.filter((post) => {
+        console.log(post);
+        return (
+          post.body.toLowerCase().includes(args.query.toLowerCase()) ||
+          post.title.toLowerCase().includes(args.query.toLowerCase())
+        );
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments;
+    },
+    me() {
+      return {
+        id: "122334322",
+        name: "Majed",
+        //  email: "majed@exmaple.com"
+      };
+    },
+    post() {
+      return {
+        id: "12344",
+        title: "The graphQL",
+        body: "test body here",
+        published: false,
+      };
+    },
+  },
 
-    Query: {    
-        users(parent, args, ctx, info){
-            if(!args.query){
-                return users
-            }
-            return users.filter(user => {
-                return user.name.toLowerCase().includes(args.query.toLowerCase())
-            })
-        },
-        posts(parent, args, ctx, info){
-            if(!args.query){
-                return posts
-            }
-                return posts.filter(post =>{
-                    console.log(post)
-                    return post.body.toLowerCase().includes(args.query.toLowerCase()) || post.title.toLowerCase().includes(args.query.toLowerCase())
-                })
-           
-            
-        },
-        comments(parent, args, ctx, info){
-            return comments
-        }
-        ,
-         me(){
-             return {
-                 id: '122334322',
-                 name: "Majed",
-                //  email: "majed@exmaple.com"
-             }
-         },
-         post(){
-             return {
-                 id: '12344',
-                 title: 'The graphQL',
-                 body: 'test body here',
-                 published: false
-             }
-         }
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id;
+      });
     },
 
-    Post : {
-        author(parent, args, ctx, info){
-           return users.find(user => {
-               return user.id === parent.author
-           })
-        }
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.author == parent.id;
+      });
     },
-    User : {
-        posts(parent, args, ctx, info){
-            return posts.filter(post =>{
-                return post.author === parent.id
-            })
-        }
-    }
-}
+  },
+};
 
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
+server.start(() => {
+  console.log("Server start running");
+});
 
-server.start(()=>{
-    console.log("Server start running")
-})
-
-// video 14 - grapgQl basics schemas and queries
